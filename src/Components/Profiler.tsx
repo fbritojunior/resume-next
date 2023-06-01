1909d4237570f0e7038f556ae317806ff3ca5f79
Francisco Brito Jr <fbritojnr@gmail.com>
2023-05-31 22:17:09 -0300

    initial architecture

---
+++ src/Components/Profiler.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faPhone, faGlobeAmericas, faExclamation, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+
+const iconMap = {
+  email: faEnvelope,
+  phone: faPhone,
+  location: faMapMarkerAlt,
+  website: faGlobeAmericas,
+  linkedin: faLinkedin,
+  github: faGithub,
+  twitter: faTwitter
+};
+
